34829b1557d60b5ba6d258eccab8fd7a3cc7651c
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-10-19 15:45:06 -0300

    first commit

---
+++ webapp/src/components/NavMenu.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavMenu: React.FC = () => {
+  return <>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Contatos</a></li>
+        </ul>
+    </nav>
+  </>;
+};
+
+export default NavMenu;
---
+++ webapp/src/pages/App.tsx
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <>
+      <footer className="rodape">
+        Copyright © 2025 Alcateia Tecnologia. Todos os direitos reservados.
+      </footer>
+    </>
+  );
+}
+
+export default App;
